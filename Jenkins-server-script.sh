decf5d17d037f7ab2cf3b2da7c1aa8db0546be0a
gunpriyak <101408401+gunpriyak@users.noreply.github.com>
2023-09-27 19:30:46 +0530

    Create Jenkins-server-script.sh

---
+++ Jenkins-server-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install jenkins
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# install terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
